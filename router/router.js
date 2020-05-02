d81d7ff24c29f871f5717c9b73951e279939121e
satyamurthy515 <satyanarayana.m@homelane.com>
2020-05-02 12:31:50 +0530

    added

--- router/router.js
+++ router/router.js
@@ -3,6 +3,8 @@
  * @param {*} fastify 
  * Router
  */
+const boom = require('boom');
+const pdf = require('html-pdf');
 
 async function Router(fastify){
 
@@ -10,6 +12,27 @@ async function Router(fastify){
         preValidation:[fastify.jwtauthentication]
     }, async (req,res)=>{
         res.status(200).send({msg: "Successfully authenticated"});
+    });
+
+
+    fastify.post('/htmltopdf', async (req, res)=>{
+        try {
+            let {html} = req.body;
+            if(!html){
+                res.status(400).send({msg:"html string is mandatory"});
+                return;
+            }
+            pdf.create(html).toBuffer(function(err, buffer){
+                if(err){
+                    res.status(500).send({msg:"Error while converting to pdf",error: err});
+                    return;
+                }
+                res.type('application/pdf');
+                res.status(200).send(buffer);
+              });
+        } catch (error) {
+            throw boom.boomify(error);
+        }
     })
 
 }
